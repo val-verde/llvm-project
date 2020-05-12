66f4c7f2a966b10fa07c383a870ce35bdf100d51
Aaditya Chandrasekhar <aaditya.c@phiar.net>
2020-05-12 01:28:26 -0700

    Add c++_shared link library to linker flags only.

--- llvm/cmake/modules/HandleLLVMStdlib.cmake
+++ llvm/cmake/modules/HandleLLVMStdlib.cmake
@@ -21,7 +21,9 @@ if(NOT DEFINED LLVM_STDLIB_HANDLED)
       check_linker_flag("-stdlib=libc++" CXX_LINKER_SUPPORTS_STDLIB)
       if(CXX_COMPILER_SUPPORTS_STDLIB AND CXX_LINKER_SUPPORTS_STDLIB)
         append("-stdlib=libc++"
-          CMAKE_CXX_FLAGS CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS
+          CMAKE_CXX_FLAGS)
+        append("-stdlib=libc++ -lc++_shared"
+          CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS
           CMAKE_MODULE_LINKER_FLAGS)
         set(LLVM_LIBCXX_USED 1)
       else()
