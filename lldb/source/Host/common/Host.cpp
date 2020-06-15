31f92af39abb78a0310ef4736392f36a368ffc68
Aaditya Chandrasekhar <aaditya.c@phiar.net>
2020-06-15 10:20:24 -0700

    Remove ANDROID check in GetModuleFileSpecForHostAddress.
    (cherry picked from commit 60a6ea6a23fc38e2449f3d5165449545f6ab8733)

--- lldb/source/Host/common/Host.cpp
+++ lldb/source/Host/common/Host.cpp
@@ -421,7 +421,6 @@ bool Host::ResolveExecutableInBundle(FileSpec &file) { return false; }
 
 FileSpec Host::GetModuleFileSpecForHostAddress(const void *host_addr) {
   FileSpec module_filespec;
-#if !defined(__ANDROID__)
   Dl_info info;
   if (::dladdr(host_addr, &info)) {
     if (info.dli_fname) {
@@ -429,7 +428,6 @@ FileSpec Host::GetModuleFileSpecForHostAddress(const void *host_addr) {
       FileSystem::Instance().Resolve(module_filespec);
     }
   }
-#endif
   return module_filespec;
 }
 
