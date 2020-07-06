4988a54a2efe233b4bdb0d9460f8db1d429faf65
Aaditya Chandrasekhar <aaditya.c@phiar.net>
2020-07-06 11:38:23 -0700

    Disable mbig-obj option for mingw targets as llvm natively supports it.

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -395,9 +395,9 @@ elseif(MINGW) # FIXME: Also cygwin?
   # Pass -mbig-obj to mingw gas on Win64. COFF has a 2**16 section limit, and
   # on Win64, every COMDAT function creates at least 3 sections: .text, .pdata,
   # and .xdata.
-  if (CMAKE_SIZEOF_VOID_P EQUAL 8)
-    append("-Wa,-mbig-obj" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
-  endif()
+  #if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+  #  append("-Wa,-mbig-obj" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+  #endif()
 endif()
 
 if( MSVC )
