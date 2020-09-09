7c495e895a55942bc8f68986f36b94215ec3fbb7
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-09-08 20:35:19 -0700

    Add missing _LIBCPP_BUILDING_LIBRARY define to libcxxabi stdlib_stdexcept.cpp, stdlib_typeinfo.cpp and cxa_handlers.cpp.
    (cherry picked from commit 68500703e17087e98070a4a49b791fa8c5464d76)

--- libcxxabi/src/stdlib_stdexcept.cpp
+++ libcxxabi/src/stdlib_stdexcept.cpp
@@ -6,6 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#define _LIBCPP_BUILDING_LIBRARY
 #include "../../libcxx/src/include/refstring.h"
 #include "stdexcept"
 #include "new"
