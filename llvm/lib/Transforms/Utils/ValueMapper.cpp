cb272240f82aa18f5aceeccc9796548cd4ac01f9
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-10-18 04:04:13 -0700

    [swift/next] [REVERT] "Reapply "OpaquePtr: Add type to sret attribute""
    This reverts commit 0a7cd99a702595ccf73c957be0127af9f25fb9a2.

--- llvm/lib/Transforms/Utils/ValueMapper.cpp
+++ llvm/lib/Transforms/Utils/ValueMapper.cpp
@@ -900,13 +900,14 @@ void Mapper::remapInstruction(Instruction *I) {
     LLVMContext &C = CB->getContext();
     AttributeList Attrs = CB->getAttributes();
     for (unsigned i = 0; i < Attrs.getNumAttrSets(); ++i) {
-      for (Attribute::AttrKind TypedAttr :
-           {Attribute::ByVal, Attribute::StructRet}) {
-        if (Type *Ty = Attrs.getAttribute(i, TypedAttr).getValueAsType()) {
-          Attrs = Attrs.replaceAttributeType(C, i, TypedAttr,
-                                             TypeMapper->remapType(Ty));
-          break;
-        }
+      if (Attrs.hasAttribute(i, Attribute::ByVal)) {
+        Type *Ty = Attrs.getAttribute(i, Attribute::ByVal).getValueAsType();
+        if (!Ty)
+          continue;
+
+        Attrs = Attrs.removeAttribute(C, i, Attribute::ByVal);
+        Attrs = Attrs.addAttribute(
+            C, i, Attribute::getWithByValType(C, TypeMapper->remapType(Ty)));
       }
     }
     CB->setAttributes(Attrs);
