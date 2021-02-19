99af50ac77622c27e637865a22034b0214cafe36
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-02-18 18:18:36 -0800

    [REVERT] Restore GetCachedPlanExplainsStop to fix build breakage.

--- lldb/include/lldb/Target/ThreadPlan.h
+++ lldb/include/lldb/Target/ThreadPlan.h
@@ -518,6 +518,10 @@ protected:
 
   virtual bool DoPlanExplainsStop(Event *event_ptr) = 0;
 
+  LazyBool GetCachedPlanExplainsStop() const {
+    return m_cached_plan_explains_stop;
+  }
+
   // This pushes a plan onto the plan stack of the current plan's thread.
   // Also sets the plans to private and not controlling plans.  A plan pushed by
   // another thread plan is never either of the above.
