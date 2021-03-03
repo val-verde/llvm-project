5d6efc02958261b1ada467c9dd8c02d281c20173
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-03-03 12:20:19 -0800

    Remove duplicate GetCachedPlanExplainsStop implementation.

--- lldb/include/lldb/Target/ThreadPlan.h
+++ lldb/include/lldb/Target/ThreadPlan.h
@@ -500,10 +500,6 @@ protected:
 
   virtual bool DoPlanExplainsStop(Event *event_ptr) = 0;
 
-  LazyBool GetCachedPlanExplainsStop() const {
-    return m_cached_plan_explains_stop;
-  }
-
   // This pushes a plan onto the plan stack of the current plan's thread.
   // Also sets the plans to private and not master plans.  A plan pushed by
   // another thread plan is never either of the above.
