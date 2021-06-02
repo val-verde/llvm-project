5a9d684dd69f7fb1cbf16853fc2e08e17f815cc6
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-06-02 15:33:56 -0700

    Add -cl-no-stdinc to libclc LLAsm build flags.

--- libclc/cmake/CMakeLLAsmInformation.cmake
+++ libclc/cmake/CMakeLLAsmInformation.cmake
@@ -1,6 +1,6 @@
 if(NOT CMAKE_LLAsm_COMPILE_OBJECT)
   set(CMAKE_LLAsm_COMPILE_OBJECT
-	  "${CMAKE_LLAsm_PREPROCESSOR} -E -P  <DEFINES> <INCLUDES> <FLAGS> -x cl <SOURCE> -o <OBJECT>.temp"
+	  "${CMAKE_LLAsm_PREPROCESSOR} -cl-no-stdinc -E -P  <DEFINES> <INCLUDES> <FLAGS> -x cl <SOURCE> -o <OBJECT>.temp"
 	  "<CMAKE_LLAsm_COMPILER> -o <OBJECT> <OBJECT>.temp")
 endif()
 
