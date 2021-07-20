76240d8b27d3e1bed6bc6bdffddcf89ff5dfc4e4
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-07-19 21:31:17 -0700

    Fix missing fallback argument parameter to VALIDATE_AND_RETURN in lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp.

--- lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp
+++ lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp
@@ -1904,7 +1904,7 @@ bool TypeSystemSwiftTypeRef::IsFunctionPointerType(
 bool TypeSystemSwiftTypeRef::IsScopedEnumerationType(lldb::opaque_compiler_type_t type) {
   auto impl = [&]() -> bool { return IsScopedEnumerationType(type); };
   VALIDATE_AND_RETURN(impl, IsFunctionPointerType, type,
-                      (ReconstructType(type)));
+                      (ReconstructType(type)), (ReconstructType(type)));
 }
 
 bool TypeSystemSwiftTypeRef::IsPossibleDynamicType(opaque_compiler_type_t type,
@@ -2171,7 +2171,7 @@ CompilerType
 TypeSystemSwiftTypeRef::GetEnumerationIntegerType(lldb::opaque_compiler_type_t type) {
   auto impl = [&]() -> CompilerType  { return GetEnumerationIntegerType(type); };
   VALIDATE_AND_RETURN(impl, GetEnumerationIntegerType, type,
-                      (ReconstructType(type)));
+                      (ReconstructType(type)), (ReconstructType(type)));
 }
 
 int TypeSystemSwiftTypeRef::GetFunctionArgumentCount(
