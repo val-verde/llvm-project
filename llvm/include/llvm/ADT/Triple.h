40574dc88e1c2022c4b2c393cd5ebed745c55ec7
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-09-23 19:25:49 -0700

    Disable android emutls default when feature level is < 29.

--- llvm/include/llvm/ADT/Triple.h
+++ llvm/include/llvm/ADT/Triple.h
@@ -849,7 +849,7 @@ public:
 
   /// Tests whether the target uses emulated TLS as default.
   bool hasDefaultEmulatedTLS() const {
-    return isAndroid() || isOSOpenBSD() || isWindowsCygwinEnvironment();
+    return (isAndroid() && isAndroidVersionLT(29)) || isOSOpenBSD() || isWindowsCygwinEnvironment();
   }
 
   /// Tests whether the target uses -data-sections as default.
