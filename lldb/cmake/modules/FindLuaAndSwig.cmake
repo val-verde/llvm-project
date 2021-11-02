be851c1193345abbfc881e90156e2da1d2511df8
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-11-02 09:49:50 -0700

    Remove restriction on lua version to be exactly 5.3.

--- lldb/cmake/modules/FindLuaAndSwig.cmake
+++ lldb/cmake/modules/FindLuaAndSwig.cmake
@@ -12,7 +12,7 @@ else()
     if (LLDB_USE_STATIC_BINDINGS)
       set(SWIG_EXECUTABLE "/not/found")
     endif()
-    find_package(Lua 5.3 EXACT)
+    find_package(Lua)
     if(LUA_FOUND AND SWIG_FOUND)
       mark_as_advanced(
         LUA_LIBRARIES
